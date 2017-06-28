21dcf516a6947739e31d0b5bd50a0a780614b972
giuseppedeponte <giuseppedeponte@gmail.com>
2017-06-28 07:27:15 +0200

    add CSS Animations on win/lose/draw

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,55 +1,48 @@
-/*global window*/
+/* eslint no-var: "off" */
 window.onload = function() {
-
   var odds = [
     // user choice is 'pierre'
-    [
-      'draw',
-      'computer',
-      'user'
-    ],
+    ['draw', 'computer', 'user'],
     // user choice is 'feuille'
-    [
-      'user',
-      'draw',
-      'computer'
-    ],
+    ['user', 'draw', 'computer'],
     // user choice is 'ciseaux'
-    [
-      'computer',
-      'user',
-      'draw'
-    ]
+    ['computer', 'user', 'draw'],
   ];
 
   var score = {
     'computer': 0,
     'user': 0,
     'draws': 0,
-    'games': 0
+    'games': 0,
   };
 
 
   // helper functions
 
   var getChoices = function(player) {
-    return document.getElementById(player).getElementsByTagName('li');
+    return document.getElementById(player)
+                   .getElementsByTagName('li');
   };
 
   var resetSelection = function(choices) {
-    var classes, i = 0;
+    var classes;
+    var i = 0;
     while (i < choices.length) {
       classes = choices[i].getAttribute('class');
 
-      classes = classes.indexOf('selected') === -1 ? classes : classes.replace('selected', '');
+      classes = classes.indexOf('selected') === -1
+                ? classes
+                : classes.replace('selected', '');
 
       choices[i].setAttribute('class', classes);
       i++;
     }
-  }
+  };
 
   var selectElement = function(id) {
-    var choices = id[0] === 'c' ? getChoices('computer') : getChoices('user')
+    var choices = id[0] === 'c'
+                  ? getChoices('computer')
+                  : getChoices('user');
 
     var element = choices[id[1]];
 
@@ -57,17 +50,19 @@ window.onload = function() {
 
     resetSelection(choices);
 
-    classes = classes.indexOf('selected') === -1 ? classes + ' selected' : classes;
+    classes = classes.indexOf('selected') === -1
+              ? classes + ' selected'
+              : classes;
 
     element.setAttribute('class', classes);
   };
 
   var updateScore = function(winner, loser, draw) {
-    if(!draw) {
-      var winH = document.getElementById(winner).clientHeight,
-        loseH = document.getElementById(loser).clientHeight,
-        totH = winH + loseH,
-        twoPercent = parseInt(totH * 0.02);
+    if (!draw) {
+      var winH = document.getElementById(winner).clientHeight;
+      var loseH = document.getElementById(loser).clientHeight;
+      var totH = winH + loseH;
+      var twoPercent = parseInt(totH * 0.02);
 
       score[winner]++;
 
@@ -82,70 +77,119 @@ window.onload = function() {
         document.getElementById(loser).style.height = loseH;
       }
     }
-    score.games++;
-    document.getElementById(winner + 'Score').innerHTML = score[winner] + '<small>/' + score.games + '</small>';
-    document.getElementById(loser + 'Score').innerHTML = score[loser] + '<small>/' + score.games + '</small>';
 
-  }
+    score.games++;
+
+    document
+        .getElementById(winner + 'Score')
+        .innerHTML = score[winner]
+                     + '<small>/'
+                     + score.games
+                     + '</small>';
+    document
+        .getElementById(loser + 'Score')
+        .innerHTML = score[loser]
+                     + '<small>/'
+                     + score.games
+                     + '</small>';
+  };
 
   var updateStatus = function(result) {
     switch (result) {
-      case "computer":
+      case 'computer':
         updateScore('computer', 'user', false);
-        //showOff('computer', 'user');
+        showOff('computer', 'user');
         break;
-      case "user":
+      case 'user':
         updateScore('user', 'computer', false);
-        //showOff('user', 'computer');
+        showOff('user', 'computer');
         break;
-      case "draw":
+      case 'draw':
         score.draws++;
         updateScore('user', 'computer', true);
+        shake();
         break;
     }
   };
-  /*
-  var showOff(winner, loser) {
-    document.getElementById(winner).getElementsByClassName('selected').style.animation = 'win .5s ease-in-out';
 
-    document.getElementById(loser).getElementsByClassName('selected').style.animation = 'lose .5s ease-in-out';
+  var shake = function() {
+    document
+        .getElementById('computer')
+        .getElementsByClassName('selected')[0]
+        .setAttribute('class', 'choice selected draw');
+
+    document
+        .getElementById('user')
+        .getElementsByClassName('selected')[0]
+        .setAttribute('class', 'choice selected draw');
 
     setTimeout(function() {
-      document.getElementById(winner).getElementsByClassName('selected').style.animation = '';
+      document
+          .getElementById('computer')
+          .getElementsByClassName('selected')[0]
+          .setAttribute('class', 'choice selected');
 
-      document.getElementById(loser).getElementsByClassName('selected').style.animation = '';
+      document
+          .getElementById('user')
+          .getElementsByClassName('selected')[0]
+          .setAttribute('class', 'choice selected');
     }, 500);
   };
-  */
+
+  var showOff = function(winner, loser) {
+    document
+        .getElementById(winner)
+        .getElementsByClassName('selected')[0]
+        .setAttribute('class', 'choice selected winner');
+
+    document
+        .getElementById(loser)
+        .getElementsByClassName('selected')[0]
+        .setAttribute('class', 'choice selected loser');
+
+    setTimeout(function() {
+      document
+          .getElementById(winner)
+          .getElementsByClassName('selected')[0]
+          .setAttribute('class', 'choice selected');
+
+      document
+          .getElementById(loser)
+          .getElementsByClassName('selected')[0]
+          .setAttribute('class', 'choice selected');
+    }, 500);
+  };
+
   // game functions
 
   // getUserChoice function
   var getUserChoice = function(id) {
     selectElement(id);
     return id.substr(1);
-  }
+  };
 
   // getComputerChoice function
   var getComputerChoice = function() {
     var choice = parseInt(Math.random() * 2.9);
     selectElement('c' + choice);
     return choice;
-  }
-
-  // update Status
-
-  userChoices = getChoices('user');
-  computerChoices = getChoices('computer');
+  };
 
   var activate = function(f) {
-    for (c in userChoices) {
-      userChoices[c].onclick = f;
+    var userChoices = getChoices('user');
+    var c = 0;
+    for ( c in userChoices ) {
+      if ( {}.hasOwnProperty.call(userChoices, c) ) {
+        userChoices[c].onclick = f;
+      }
     }
   };
 
   var play = function() {
-    var id = this.getAttribute('id'),
-      result;
+    var id = this.getAttribute('id');
+    var userChoice;
+    var computerChoice;
+    var result;
     activate('');
     resetSelection(getChoices('computer'));
     userChoice = getUserChoice(id);
@@ -158,12 +202,10 @@ window.onload = function() {
           resetSelection(getChoices('computer'));
           resetSelection(getChoices('user'));
           activate(play);
-        }, 1000);
+        }, 750);
       },
       1000
     );
   };
-
   activate(play);
-
 };
